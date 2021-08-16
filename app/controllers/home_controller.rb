0f5943d1634f6f4b6903faff4222f5c3dd9a1209
temyramdhan6740 <temyramdhan6740@gmail.com>
2021-08-16 10:06:05 +0700

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,33 @@
 class HomeController < ApplicationController
     def index
-        @var = 'index'
-        @table = CbiBlog.all
+        @table = CbiBlog.all.order(updated_at: :desc)
     end
  
     def create
-        @var = "create"
+        @rand = SecureRandom.urlsafe_base64(nil, false)
         @new = CbiBlog.new
     end
 
     def create_prog
-        
+        @table = CbiBlog.new(form_params)
+        if @table.save
+            redirect_to root_path, notice: 'Create blog success'
+        else
+            render :create
+        end
     end
  
     def update
-        
+        @id = CbiBlog.find(params[:blog_id])        
+    end
+
+    def update_prog
+        @id = CbiBlog.find(params[:getId])
+        if @id.update(form_params)
+            redirect_to root_path, notice: 'Update blog success'
+        else
+            render :update
+        end
     end
  
     def show
@@ -22,13 +35,27 @@ class HomeController < ApplicationController
     end
  
     def destroy
-        
+        @table = CbiBlog.find(find_params)
+        if @table.destroy
+            redirect_to root_path, notice: 'Delete '+ find_params() +' success'
+        else
+            render :index
+        end
     end
  
     private
- 
     def find_blog
         Blog.find(params[:id])
+    end
+
+    private
+    def form_params
+        params.require(:cbi_blog).permit(:blog_id, :blog_title, :blog_desc)
+    end
+
+    private
+    def find_params
+        params[:getId]
     end
  end
  
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   get '/create', to: 'home#create'
   post '/create/progress', to: 'home#create_prog', as: 'createProgress'
+  get '/:getId/edit', to: 'home#update', as: 'edit'
+  patch '/:getId/edit/progress', to: 'home#update_prog', as: 'updateProgress'
+  delete '/:getId/delete', to: 'home#destroy', as: 'destroyProgress'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
