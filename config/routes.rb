72a8532a6e52894023dbb8293535702c0aae281b
temyramdhan6740 <temyramdhan6740@gmail.com>
2021-08-16 10:30:37 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   get '/create', to: 'home#create'
   post '/create/progress', to: 'home#create_prog', as: 'createProgress'
-  get '/:getId/edit', to: 'home#update', as: 'edit'
+  get '/:blog_id/edit', to: 'home#update', as: 'edit'
   patch '/:getId/edit/progress', to: 'home#update_prog', as: 'updateProgress'
   delete '/:getId/delete', to: 'home#destroy', as: 'destroyProgress'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
